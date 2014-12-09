2c6d8bca251115d6c469d05d58e75f1bd2dd9da4
Tagl <tagl@tagl.is>
2014-12-09 20:53:42 +0000

    To test the database

--- Verklegt2014/Verklegt2014.cpp
+++ Verklegt2014/Verklegt2014.cpp
@@ -1,11 +1,12 @@
 // Verklegt2014.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include "Database.h"
 #include "ConsoleUI.h"
 
 int main(int argc, char *argv[])
 {
-
+    Database db;
     ConsoleUI cui = ConsoleUI();
 	cui.mainMenu();
 	return 0;
