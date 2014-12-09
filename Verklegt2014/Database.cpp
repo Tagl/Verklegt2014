6fd489266c9228c48d401504b0d62e6850999d28
huldar <huldarbjarmi@gmail.com>
2014-12-09 19:31:53 +0000

    small fixes

--- Verklegt2014/Database.cpp
+++ Verklegt2014/Database.cpp
@@ -8,7 +8,7 @@ Database::Database()
     if(db.isOpen())
     {
         QSqlQuery query;
-        query.exec("CREATE TABLE IF NOT EXISTS Computers (ID INT PRIMARY KEY  NOT NULL, Name VARCHAR, BuildingYear INT, WasItBuild VARCHAR, Description TEXT);");
+        query.exec("CREATE TABLE IF NOT EXISTS Computers (ID INT PRIMARY KEY  NOT NULL, Name VARCHAR,ComputerType VARCHAR, YearBuilt INT, WasMade VARCHAR, Description TEXT);");
         query.exec("CREATE TABLE IF NOT EXISTS Persons (ID INT PRIMARY KEY NOT NULL, FirstName VARCHAR, SurName VARCHAR, DoB DATE, DoD DATE, Gender CHAR);");
         query.exec("CREATE TABLE IF NOT EXISTS Connections(P_ID INT NOT NULL, C_ID INT NOT NULL, PRIMARY KEY (P_ID,C_ID), FOREIGN KEY (C_ID) REFERENCES Computers(ID), FOREIGN KEY (P_ID) REFERENCES Persons(ID));");
     }
