057c431d967086357e1734f22547bdf6a87804b1
Tagl <tagl@tagl.is>
2014-12-15 20:05:45 +0000

    Basic GUI added

--- Verklegt2014/Verklegt2014.cpp
+++ Verklegt2014/Verklegt2014.cpp
@@ -1,15 +1,17 @@
 // Verklegt2014.cpp : Defines the entry point for the console application.
 //
+#include <QApplication>
 #include <iostream>
+#include "mainwindow.h"
 #include "Database.h"
 #include "ConsoleUI.h"
 
 int main(int argc, char *argv[])
 {
-    Database db;
-    ConsoleUI cui = ConsoleUI();
-	cui.mainMenu();
-    db.getDB().close();
-	return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
 
