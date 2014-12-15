413a0dfc4eadb727028a41f752cc9f8a6d423dab
Tagl <tagl@tagl.is>
2014-12-15 21:06:31 +0000

    Working on displaying table

--- Verklegt2014/mainwindow.cpp
+++ Verklegt2014/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "PersonService.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,29 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_scientistSearch_textChanged(const QString &arg1)
+{
+
+}
+
+void MainWindow::displayComputers()
+{
+
+}
+
+void MainWindow::displayScientists()
+{
+    PersonService serv;
+    ui->scientistTable->clear();
+    std::vector<Person> people = serv.getPeople();
+    ui->scientistTable->setRowCount(people.size());
+    for(int i = 0; i < people.size(); i++)
+    {
+        Person p = people.at(i);
+        QString scientistFirst = QString::fromStdString(p.firstname);
+        QString scientistSurname = QString::fromStdString(p.surname);
+        QString scientistGender = p.gender == MALE ? "Male" : "Female";
+    }
 }
--- Verklegt2014/mainwindow.h
+++ Verklegt2014/mainwindow.h
@@ -14,6 +14,11 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_scientistSearch_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
+    void displayScientists();
+    void displayComputers();
 };
